032b4d543ed4374a73b6ba76d7142ace8e8a1087
khai96_ <hvksmr1996@gmail.com>
2016-09-24 18:00:02 +0700

    Sửa nhập, xuất 2.4

--- cpp/2.4.cpp
+++ cpp/2.4.cpp
@@ -18,9 +18,11 @@ int main ();
 
 // Hàm nhập
 void nhap (DoVat * & mang, unsigned & soluong, unsigned & succhua);
+void nhap (DoVat & dovat);
 
 // Hàm xuất
-void xuat (DoVat * mang);
+void xuat (DoVat * mang, unsigned soluong);
+void xuat (DoVat dovat);
 
 // Hàm Chọn đồ vật
 void chondovat (DoVat * & dich, unsigned & soluongdich, unsigned succhua, DoVat * nguon, unsigned soluongnguon);
@@ -41,20 +43,31 @@ int main () {
   return 0;
 }
 
+void nhap (DoVat * & mang, unsigned & soluong, unsigned & succhua) {
+  cout << "Nhap so luong cac do vat: ";
+  cin >> soluong;
+  cout << "Nhap suc chua cua tui dung: ";
+  cin >> succhua;
+  mang = new DoVat[soluong];
+  cout << "Nhap lan luot khoi luong va gia tri cua tung do vat: ";
+  for (unsigned i = 0; i != soluong; ++i) nhap(mang[i]);
+}
+
+void nhap (DoVat & dovat) {
+  cin >> dovat.khoiluong >> dovat.giatri;
+}
+
+void xuat (DoVat * mang, unsigned soluong) {
+  cout << "Cac do vat da chon (GiaTri / KhoiLuong):\n";
+  for (unsigned i = 0; i != soluong; ++i) xuat(mang[i]);
+}
+
+void xuat (DoVat dovat) {
+  cout << " + " << dovat.giatri << " / " << dovat.khoiluong << endl;
+}
+
 void chondovat (DoVat * & dich, unsigned & soluongdich, unsigned succhua, DoVat * nguon, unsigned soluongnguon) {
   dich = new DoVat[soluongnguon];
   soluongdich = 0; // Khởi tạo
   // YOUR TEXT HERE...
-}
-
-void nhap (DoVat * & mang, unsigned & soluong, unsigned & succhua) {
-  cout << " Nhap so luong : ";
-  cin >> soluong;
-  cout << " Nhap suc chua : ";
-  cin >> succhua;
-  mang = new int[soluong];
-  for(int i = 0; i != soluong; ++i) {
-    cout << "Nhap khoi luong do vat thu "<<i<<" : ";
-    cin >> mang[i];
-  }
 }
