544b0c61ec1138b8eda6d6f0bc777b669a51b46a
khai96_ <hvksmr1996@gmail.com>
2016-09-24 16:10:17 +0700

    Tạo 2.9

---
+++ cpp/2.9.cpp
@@ -0,0 +1,24 @@
+// Dùng C-Free 5
+// Giải thuật Tham lam
+
+#include <iostream> // cin, cout, endl
+#include <algorithm> // swap
+
+using namespace std;
+
+/* CÁC HÀM CẦN XÂY DỰNG */
+
+// Chương trình chính
+int main ();
+
+// Nhập, xuất
+void nhap (unsigned & sotien, unsigned & * menhgia, unsigned & soloai);
+void xuat (unsigned sotien, unsigned * menhgia, unsigned * soto, unsigned soloai);
+
+// Chia tiền
+void chiatien (unsigned & * soto, unsigned * menhgia, unsigned soloai, unsigned sotien);
+
+// Tiện ích
+void sapxep (unsigned * menhgia, unsigned soluong);
+
+/* NỘI DUNG */
